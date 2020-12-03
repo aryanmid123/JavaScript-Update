8bad64f2e766ad4bde9b7a9dca9bf34b0b2aeb99
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-03 12:18:02 +0530

    Create calculatediscount1.js
    output shall be 75% off

---
+++ calculatediscount1.js
@@ -0,0 +1,6 @@
+var sellingprice=199;
+var litingprice=799;
+var discountpercent=((listingprice-sellingprice)/listingprice)*100;
+console.log("Discount percentage is:"+ discountpercent);
+displayDiscountPercentage=Math.round(discountPercent);
+console.log(displayDiscountPercentage +"%off);
