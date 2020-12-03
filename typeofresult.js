775fd2651e61857557be1e87c6b5394213180f1a
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-03 12:24:57 +0530

    Create typeofresult.js
    we are using type off keyword here

---
+++ typeofresult.js
@@ -0,0 +1,8 @@
+var sellingprice=199;
+var listingprice=799;
+var discountpercent=((listingprice-selllingprice)/listingprice)*100;
+console.log("Discount Percentage is:"+discountPercent);
+displayDiscountPercentage=Math.round(discountPercent);
+console.log(displayDiscountPercentage +"%off);
+var result=listingprice>sellingprice;
+console.log(typeof result);
