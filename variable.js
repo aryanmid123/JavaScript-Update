3cf569c1c1a2f007daffb038383f735b59727b50
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-03 11:33:09 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,10 @@
+var fullName="Aryan Middlagajani"
+var courseName="Javascript Update";
+var isLoggedIn = false;
+var loggedCount =34;
+console.log(loggedCount);
+console.log("loggedCount);
+var paymentMode;
+paymentMode="credit card";
+
+
