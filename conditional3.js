269bd5bd47a5615a16a83f877b8f03bea0b9119a
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-03 12:40:28 +0530

    Create conditional3.js
    output shall be" Its really HOT outside"

---
+++ conditional3.js
@@ -0,0 +1,15 @@
+var temperature
+//TODO:Go to google and get the data 
+temperature=49;
+if(temperature<20)
+{
+console.log("its very cold outside");
+}
+if(temperature<30)
+{
+console.log("Its moderate outside");
+}
+else
+{
+console.log("Its really HOT outside")
+}
