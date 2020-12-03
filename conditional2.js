c13fab634904a6c71576eb15d9feb4e40624bbf3
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-03 12:36:26 +0530

    Create conditional2.js
    output shall be Its moderate outside

---
+++ conditional2.js
@@ -0,0 +1,11 @@
+var temperature
+//TODO:Go to google and get the data 
+temperature=29;
+if(temperature<20)
+{
+console.log("its very cold outside");
+}
+if(temperature<30)
+{
+console.log("Its moderate outside");
+}
