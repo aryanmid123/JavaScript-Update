6b4c67ae15e564a1c953f5a603255cf21e54d7e1
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-03 11:46:48 +0530

    Create usersignup.js

---
+++ usersignup.js
@@ -0,0 +1,16 @@
+const uid="abc123";
+var fullName="Aryan Middlagajani";
+var email="aryanmiddlagajani@gmail.com";
+var password="123456";
+var confirmPassword ="123456";
+var CourseCount=0;
+var isLoggedInFromGoogle=false;
+console.log(uid);
+console.log("Full Name is:",fullName);
+console.log(email);
+console.log('
+             With Unique ID:${uid}
+             User is :${fullName}
+             and his email is : ${email}
+             and uses his password:${password}
+             ');
