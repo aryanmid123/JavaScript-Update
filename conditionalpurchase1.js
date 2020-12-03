925aff02a289121de67443dc33cecd84e1c7d0a7
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-03 12:59:45 +0530

    Create conditionalpurchase1.js

---
+++ conditionalpurchase1.js
@@ -0,0 +1,28 @@
+Here
+//user is only allowed to make a purchase when he is:
+
+//logged in 
+//email-verified
+//cardInfo-valid
+
+//If any one is missing ,stop purchase 
+
+var isLoggedIn=true;
+var isEmailverified=false;
+var CardInfo -true;
+
+//if(isLoggedIn)
+{
+console.log("Logged IN Success");
+if(isEmailVerified)
+{
+console.log("Email is verified");
+then 
+
+if (isLogged in && isEmailVerified && cardInfo)
+{
+console.log("Allow user to make a purchase");
+}
+else{
+console.log("You are NOT allowed to do that");
+}
