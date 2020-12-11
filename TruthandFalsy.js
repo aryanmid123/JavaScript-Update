993c92916d31cee95eb898586dbde49a8823f886
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-11 11:36:09 +0530

    Create TruthandFalsy.js
    Output shall be Condition is true

---
+++ TruthandFalsy.js
@@ -0,0 +1,16 @@
+//TODO:falsy
+undefined[Falsy values]
+null     [Falsy values]
+0        [Falsy values]
+"        [Falsy values]
+NaN      [Falsy values]
+
+
+
+
+
+var user=null;
+if(user)
+{
+console.log("Condition is true");
+}
