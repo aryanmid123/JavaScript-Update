af635bff6aa5b67a49ac0f471c0b84c4b8a72e64
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-11 12:52:02 +0530

    Create windowoperator.js
    O/P shall be A true Statement

---
+++ windowoperator.js
@@ -0,0 +1,17 @@
+In Browser Type This 
+
+var myName="Aryan";
+var myName="Aryan";
+myName
+<<"Aryan"
+if(myName==myName)
+{
+console.log("A true statement")
+}
+if(myName ==window.myName)
+{
+console.log("A true statement")
+}
+ 
+ 
+// Window.myName is a Context
