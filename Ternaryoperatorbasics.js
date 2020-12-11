701111ce01256836d68199ffa7557b6ace592d9b
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-11 11:18:29 +0530

    Create Ternaryoperatorbasics.js
    output shall be show signout button

---
+++ Ternaryoperatorbasics.js
@@ -0,0 +1,12 @@
+//Show user a signout button if he is authenticated 
+//otherwise show him option to login/signup
+
+var authenticated=true;
+if(authenticated)
+{
+console.log("show signout button")
+}
+else
+{
+console.log("show login option");
+}
