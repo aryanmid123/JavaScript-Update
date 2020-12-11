9798b8c989c33d1e83564f48a9cbd7ce293b023e
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-11 13:56:57 +0530

    Create ScopeChaining02.js
    O/P shall be 
    Line number 3 Aryan
    Line number 7 MyA
    Line number 11 Mr A D

---
+++ ScopeChaining02.js
@@ -0,0 +1,13 @@
+var name ="Aryan";
+console.log("Line number 3",name);
+function sayName()
+{
+var name ="My A";
+console.log("Line number 7",name);
+sayNameTwo();
+function sayNameTwo()
+{
+var name="Mr.A D"
+console.log("Line number 11";,name);
+}
+}
