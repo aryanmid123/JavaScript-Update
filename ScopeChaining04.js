35e40fe379bae6cde5ece0efc5b93c84b067a615
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-11 13:59:36 +0530

    Create ScopeChaining04.js
    O/P shall be 
    Line number 3 Aryan
    Line number 7 Aryan 
    Line number 11 Aryan

---
+++ ScopeChaining04.js
@@ -0,0 +1,16 @@
+//var name ="Aryan";
+console.log("Line number 3",name);
+function sayName()
+{
+//var name ="My A";
+console.log("Line number 7",name);
+sayNameTwo();
+function sayNameTwo()
+{
+//var name="Mr.A D"
+console.log("Line number 11";,name);
+}
+}
+
+
+//If you comment all the variables then the output shall be 
