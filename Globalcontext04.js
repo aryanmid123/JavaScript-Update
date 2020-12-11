4dc0e8f576b9d5f850e6dda121b8f5359d1d7aa5
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-11 13:07:04 +0530

    Create Globalcontext04.js

---
+++ Globalcontext04.js
@@ -0,0 +1,15 @@
+var num=2;
+function sayMe()
+{
+console.log("say;me");
+}
+sayMe()
+
+
+//Global Context is running behind 
+
+Mostly Window object is tied majorly with browser 
+
+
+1) Function declarations are scanned and made availaible
+2) Variable declarations are scanned and made undefined (like method)
