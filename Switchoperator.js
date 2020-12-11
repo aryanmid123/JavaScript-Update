b9668a89ac566e8eb095a53782c63dad7eaa77b2
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-11 11:31:22 +0530

    Create Switchoperator.js
    When so many cases are there that time switch cases must be used 
    And also the break statement dips down (return the break statement)

---
+++ Switchoperator.js
@@ -0,0 +1,25 @@
+//create an application with following roles;
+//admin-gets full access
+//subadmin-gets access to create/delete courses
+//testprep-gets access to create/delete tests
+//user-gets access to consume content
+
+var user="testprep";
+switch(user)
+{
+case "admin";
+console.log("You get full access");
+break;
+case "subadmin";
+console.log("gets access to create/delete courses");
+break;
+case "testprep";
+console.log("gets access to create/delete tests");
+break;
+case "subadmin";
+console.log("gets access to create/delete courses");
+break;
+default:
+console.log("Trial user");
+break;
+}
