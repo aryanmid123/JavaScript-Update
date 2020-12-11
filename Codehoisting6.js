4f60bfb9d8329e3ee6ef4a6cd5b7b28efcc48674
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-11 13:27:23 +0530

    Create Codehoisting6.js
    Global context never no about the back function 
    Here the output shall be bigTipper is not a function

---
+++ Codehoisting6.js
@@ -0,0 +1,12 @@
+tipper("80")
+function tipper(a)
+{
+var bill =parseInt(a);
+console.log(bill+5);
+}
+bigTipper("200");
+var bigTipper=function(a)
+{
+var bill=parseInt(a);
+console.log(bill+15);
+};
