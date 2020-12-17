cb4dcbfc12ab592291754e30b557f409472c71e9
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-17 23:48:22 +0530

    Create arrayunshift.js

---
+++ arrayunshift.js
@@ -0,0 +1,9 @@
+var user=["Aryan","mid23@gmail.com",3,34,true];
+console.log(user);
+user.pop();
+user.pop();
+console.log(user);
+user.unshift("NEW VALUE");
+console.log(user);
+user.shift();
+console.log(user);
