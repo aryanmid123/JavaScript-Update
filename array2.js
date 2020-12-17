a1feedeb5e6969fc2ccef3ece092fdc38b7d4cf0
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-17 23:39:19 +0530

    Create array2.js
    Delhi is the output

---
+++ array2.js
@@ -0,0 +1,5 @@
+var countries=["India","USA","Japan","Russia"];
+var states=new Array("Rajasthan","Delhi","Mumbai","Assam")
+console.log(states[]);
+console.log(states[1]);
+console.log(states.length);
