e58aa58037a47b4960488697ccbf753875422dec
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-17 23:44:51 +0530

    Create array4.js

---
+++ array4.js
@@ -0,0 +1,7 @@
+//suppose we need to delete last through elements we are using pop
+
+var user=["Aryan","mid23@gmail.com",3,34,true];
+console.log(user);
+user.pop();
+user.pop();
+console.log(user);
