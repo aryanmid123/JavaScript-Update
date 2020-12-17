664404e142581937eb0ca34362ccd91e07c8b9c0
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-17 23:41:28 +0530

    Create array3.js
    Punjab,Delhi,Mumbai,Assam

---
+++ array3.js
@@ -0,0 +1,6 @@
+var countries=["India","USA","Japan","Russia"];
+var states=new Array("Rajasthan","Delhi","Mumbai","Assam")
+console.log(states[1]);
+console.log(states.length);
+states[0]="Punjab";
+console.log(states);
