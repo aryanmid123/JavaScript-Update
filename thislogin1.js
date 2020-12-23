b78e4ed37f213c77f25475efa976751521cad5dd
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-23 23:11:38 +0530

    Create thislogin1.js
    {}
    LINE 7{
    firstName:Aryan
    CourseCount:4
    getCourseCount:
    Function:getCourseCount]
    }

---
+++ thislogin1.js
@@ -0,0 +1,10 @@
+var user={
+         firstName:"Aryan"
+         Coursecount:4,
+         getCourseCount:function(){
+         console.log("LINE 7",this);
+         };
+         };
+         user.getCourseCount(); //This is not a regular function call.
+         //In browser there will be always Window object
+     
