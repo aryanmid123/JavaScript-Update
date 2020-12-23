2e6dd6001b6d64315dbb294d5e9af3e1d2b428a5
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-23 15:29:53 +0530

    Create Objects5.js

---
+++ Objects5.js
@@ -0,0 +1,12 @@
+console.table(user);
+
+
+
+index               values
+
+firstName:          Aryan
+lastName:           Middlagajani
+role                Admin
+loginCount          44
+facebookSignedIn    true
+         
