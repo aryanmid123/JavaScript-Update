0ed37399db699b14805d7b69606a1f4c72cec997
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-23 15:45:31 +0530

    Create Methodobjects1.js
    Aryan is enrolled in total of 2 courses

---
+++ Methodobjects1.js
@@ -0,0 +1,6 @@
+var courseList=true;
+console.log(user.firstName);
+console.log(user.getCoursecount());
+user.buyCourse("ReactJS course");
+user.buyCourse("Angular Course");
+console.log(user.getCourseCount());
