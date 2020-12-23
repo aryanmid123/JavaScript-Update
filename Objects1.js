5ef6179b024102882ea0d889d3e4da7fd7241ec6
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-23 15:20:42 +0530

    Create Objects1.js
    Aryan Middlagajani

---
+++ Objects1.js
@@ -0,0 +1,9 @@
+var user={
+         firstName:"Aryan";
+         lastName:"Middlagajani";
+         role:"Admin";
+         loginCount:32;
+         facebookSignedIn:true;
+         }
+         console.log(user.firstName);
+         console.log(User["lastName"]); //Not a good practice 
