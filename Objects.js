ebee4e3c2d59bf7b0b58a1bae93468bfdc712eed
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-23 15:18:37 +0530

    Create Objects.js
    Aryan

---
+++ Objects.js
@@ -0,0 +1,10 @@
+//spinal cord of javascript these are optimized than arrays 
+
+var user={
+         firstName:"Aryan";
+         lastName:"Middlagajani";
+         role:"Admin";
+         loginCount:32;
+         facebookSignedIn:true;
+         }
+         console.log(user.firstName);
