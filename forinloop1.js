74313efe0f3484cc77f751db2daf1bb7dd6281c3
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-23 23:04:38 +0530

    Create forinloop1.js
    Key is: yt and value is:Youtube
    Key is: ig and value is: Instagram
    Key is: fb and value is: Facebook
    Key is: tw and value is: Twitter

---
+++ forinloop1.js
@@ -0,0 +1,12 @@
+const symbols={
+               yt:"Youtube"
+               ig:"Instagram"
+               fb:"Facebook"
+               tw:"Twitter"
+               };
+               
+               for(const n in symbols)
+               {
+               console.log('Key is:${n} and value is :${symbols[n]}');
+               }
+               
