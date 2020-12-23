de46315247b61c444692737716f87c1071aeabdc
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-23 22:49:09 +0530

    Create foreachloop.js
    Rajasthan"
                   "Delhi"
                   "Assam"
                   1947
                   "Tamilnadu"
                   "Maharashtra"

---
+++ foreachloop.js
@@ -0,0 +1,10 @@
+const myStates=[
+               "Rajasthan"
+               "Delhi"
+               "Assam"
+               1947
+               "Tamilnadu"
+               "Maharashtra"
+               ];
+               
+               myStates.forEach ( (s)=>(console.log(s)));
