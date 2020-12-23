bf88a3d0864f507a8dbd4a250913d6ac2fcdb0ab
aryanmid123 <36022695+aryanmid123@users.noreply.github.com>
2020-12-23 22:56:21 +0530

    Create foroffloop.js
    Youtube Facebook Instagram Netflix Amazon

---
+++ foroffloop.js
@@ -0,0 +1,13 @@
+//one will be used majorly with arrays and one will be used majorly with objects.
+
+const names=["Youtube","Facebook","Instagram","Netflix","Amazon"];
+for(const n of names)
+{
+console.log(n);
+}
+
+
+
+
+//For off is used for Arrays
+//For in is used for objects
